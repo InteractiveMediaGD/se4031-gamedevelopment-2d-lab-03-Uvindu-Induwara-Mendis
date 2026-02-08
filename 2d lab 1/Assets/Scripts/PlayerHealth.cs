b11cb118c32d084ec89c82217780b8b885fe148a
Uvindu Induwara Mendis <it22177100@my.sliit.lk>
2026-02-08 07:13:47 +0530

    Fixed project setup and .gitignore

---
+++ 2d lab 1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public TMP_Text healthText;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateUI();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0) currentHealth = 0;
+
+        UpdateUI();
+
+        if (currentHealth == 0)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+        Time.timeScale = 1f;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        healthText.text = "Health: " + currentHealth;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            TakeDamage(10);
+        }
+    }
+}
